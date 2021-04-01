1c37f632b32feb2e0c0359296cc25eb229308bc5
Paul <apps@fullmeter.com>
2021-04-01 13:45:05 +0100

    server now contains room data, users can both pick and ban characters in the correct order

--- server/app.js
+++ server/app.js
@@ -22,13 +22,17 @@ const getRandomInt = (min, max) => {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min) + min);
 }
+const tickers = {
+  8: ["B1", "B2", "P1", "P2", "P2", "P1", "Q"]
+}
+const roomsData = {}
 
 
 io.on("connection", (socket) => {
   console.log("New client connected");
   socket.userName = `${socket.handshake.headers.username}#${getRandomInt(1000, 10000)}`;
   socket.roomCode = socket.handshake.headers.roomcode ? socket.handshake.headers.roomcode : crypto.randomBytes(3).toString('hex').toUpperCase();
-
+  
   
   const users = [];
   for (let [id, socket] of io.of("/").sockets) {
@@ -38,38 +42,76 @@ io.on("connection", (socket) => {
       roomCode: socket.roomCode,
     });
   }
-  const requstedRoomCode = io.of("/").adapter.rooms.get(socket.roomCode);
+
+  const requstedRoomCode = io.of("/").adapter.rooms.get(socket.roomCode); 
+
+  console.log(roomsData);
   
   if (requstedRoomCode && requstedRoomCode.size > 1) {
     // there's too many people in the room!
     console.log("There's too many people in the room!")
     socket.roomCode = "";
     socket.emit("event://room-full", "soz");
+
   } else if (requstedRoomCode && requstedRoomCode.size === 1) {
-    // let the opponent know someone has arrived
+
+    // fetch the waiting user's data, assign pick position and join the room
     const waitingUser = users.filter(userObj => userObj.roomCode === socket.roomCode && userObj.userName !== socket.userName);
+    roomsData[socket.roomCode].playerTwo ? roomsData[socket.roomCode].playerOne = socket.id : roomsData[socket.roomCode].playerTwo = socket.id;
     console.log("you've joined a room");
     socket.emit("event://room-joined", {userName: socket.userName, roomCode: socket.roomCode, opponentName: waitingUser[0].userName});
-    socket.to(socket.roomCode).emit('event://opponent-joined', {opponentName: socket.userName});
     socket.join(socket.roomCode);
-    io.to(socket.roomCode).emit('data://draft-characters', ["Ryu", "Ken", "Akuma", "Guile", "Abigail", "Chun-Li", "Blanka", "Ed"]);
-    if (getRandomInt(1, 3) === 1) {
-      socket.emit("request://ban-character");
-    } else {
-      socket.to(socket.roomCode).emit("request://ban-character");
-    }
+
+    // let the opponent know someone has arrived and join the room
+    socket.to(socket.roomCode).emit('event://opponent-joined', {opponentName: socket.userName});
+    
+
+    // send both players the draft list and tell player 1 to choose a ban
+    io.to(socket.roomCode).emit('data://draft-characters', roomsData[socket.roomCode].draftCharacters);
+    io.to(roomsData[socket.roomCode].playerOne).emit("request://ban-character");
+    roomsData[socket.roomCode].tickerPosition++;
+
   } else {
     // first person to join the room, wait for an opponent
     console.log("you're the first person in the room")
     socket.join(socket.roomCode);
     socket.emit("event://room-created", {userName: socket.userName, roomCode: socket.roomCode});
+
+    // set up the room data
+    roomsData[socket.roomCode] = {
+      "draftCharacters": ["Ryu", "Ken", "Akuma", "Guile", "Abigail", "Chun-Li", "Blanka", "Ed"],
+      "tickerPosition": 0,
+      "playerOne": "",
+      "playerTwo": "",
+    }
+
+    // decide whether it's player one or player two
+    getRandomInt(1, 3) === 1 ? roomsData[socket.roomCode].playerOne = socket.id : roomsData[socket.roomCode].playerTwo = socket.id;
+
   }
 
-  // a user has banned a character
-  // let the other user know which character, and request a ban from them
-  socket.on("user://ban-character", (characterName) => {
-    socket.to(socket.roomCode).emit('data://banned-character', characterName);
-    socket.to(socket.roomCode).emit('request://ban-character');
+  // a user has banned a character. Let the other user know
+  // which character, and request an action from the next player
+  socket.on("user://select-character", (data) => {
+    console.log(data)
+    socket.to(socket.roomCode).emit(`data://${data.selectionType}-character`, data.characterName);
+    
+    const nextAction = tickers[8][roomsData[socket.roomCode].tickerPosition];
+
+    if (nextAction.includes("B")) {
+      if (nextAction.includes("1")) {
+        io.to(roomsData[socket.roomCode].playerOne).emit("request://ban-character");
+      } else {
+        io.to(roomsData[socket.roomCode].playerTwo).emit("request://ban-character");
+      }
+    } else if (nextAction.includes("P")) {
+      if (nextAction.includes("1")) {
+        io.to(roomsData[socket.roomCode].playerOne).emit("request://pick-character");
+      } else {
+        io.to(roomsData[socket.roomCode].playerTwo).emit("request://pick-character");
+      }
+    }
+    roomsData[socket.roomCode].tickerPosition++;
   })
   
   
